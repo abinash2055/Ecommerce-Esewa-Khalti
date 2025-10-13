29b4b91d02fa12f9c7a3fa44ad447d0cb9c60de6
Abinash Nath Pandey <abinashnathpandey@gmail.com>
2025-10-13 19:59:29 +0545

    Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./module/common/Navbar";
+import Footer from "./module/common/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   );
 }
